5d95210bb5c139bde6644262d0232dae212f0b4e
Thatdanai <6714140025@mut.ac.th>
2026-07-02 16:06:38 +0700

    update to remote

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     echo "Hello Git Hub";
     echo "My name is Thatdanai Kub";
     echo "I don't know what am I doing";
+
+    echo "Computer Engineering Embedded";
+    echo "Oh my goodness net phom dee mak";
 ?>
